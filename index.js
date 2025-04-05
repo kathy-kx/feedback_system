6b748bee6c1371f7bf2cd56a9ea28e7f5326b392
kxzhu <zhukexin1993@qq.com>
2025-04-05 10:57:00 -0400

    add logs

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5001; // Look at the underlying environment and see if they have declared a port for us to use.
 // app.listen(5001); // Start the server and listen on port 5000. The server will listen for incoming requests on this port.
+
+console.log(`Received PORT from environment: ${process.env.PORT}, Effective PORT: ${PORT}`);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`); // Log a message to the console indicating that the server is running and on which port.
 });
